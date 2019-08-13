bfa38a06ef5bf93578513a54f1bd61c6fc713d1d
Vectormike <victorjonah199@gmail.com>
2019-08-13 18:49:53 +0100

    Rebuild

--- src/components/Index/indexPage.jsx
+++ src/components/Index/indexPage.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./indexPage.css";
 
-class Index extends Component {
+class IndexPage extends Component {
   constructor() {
     super();
     this.state = {
@@ -15,7 +16,7 @@ class Index extends Component {
       const image = await response.json();
       this.setState({ image: image.message });
     } catch (error) {
-      console.log(` Error b2=> ${error}`);
+      console.log(` Error fetching`);
     }
   };
 
@@ -25,21 +26,67 @@ class Index extends Component {
 
   render() {
     return (
+      // <div
+      //   className="index"
+      //   style={{ backgroundImage: `url(${this.state.image})` }}
+      // >
+      //   <nav className="navbar">
+      //     <a className="navbar-brand" href="to">
+      //       <i className="fas fa-dog" />
+      //     </a>
+      //   </nav>
+      //   <nav className="navbar button fixed-bottom">
+      //     <button className="btn first">View Gallery</button>
+      //   </nav>
+      // </div>
       <div>
-        <div
+        <header>
+          <a href="to" id="logo">
+            <i className="fas fa-dog" />
+          </a>
+        </header>
+        <section
           style={{ backgroundImage: `url(${this.state.image})` }}
+          id="hero1"
+          className="hero"
         >
-          <nav className="navbar fixed-bottom navbar-light">
-            <Link className="navbar-brand" to="to">
-              <button type="button" className="btn btn-primary">
-                View
-              </button>
-            </Link>
-          </nav>
-        </div>
+          <div className="inner">
+            <div className="copy">
+              <h1>Choice mountains bro</h1>
+              <p>
+                Its like im actually there! But sitting at a computer. Wow, the
+                future is intense!
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="content">
+          <div className="inner">
+            <div className="copy">
+              <h1>Amazing copy!</h1>
+              <p>The words are like jumping out at me man! </p>
+            </div>
+          </div>
+        </section>
+        <section id="hero2" className="hero">
+          <div className="inner">
+            <div className="copy">
+              <h1>Dem some big ass caves</h1>
+              <p>You could probably fit in there ;)</p>
+            </div>
+          </div>
+        </section>
+        <section className="content">
+          <div className="inner">
+            <div className="copy">
+              <h1>An inspiring quote</h1>
+              <p>/-- file not found --/</p>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
 }
 
-export default Index;
+export default IndexPage;

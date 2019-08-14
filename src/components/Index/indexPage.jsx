1f7635975a0531a0bfcee86ba504824ea44c3ecf
Vectormike <victorjonah199@gmail.com>
2019-08-14 12:59:43 +0100

    Lost in this shit!

--- src/components/Index/indexPage.jsx
+++ src/components/Index/indexPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./indexPage.css";
 
+
 class IndexPage extends Component {
   constructor() {
     super();
@@ -15,6 +16,7 @@ class IndexPage extends Component {
       const response = await fetch("https://dog.ceo/api/breeds/image/random");
       const image = await response.json();
       this.setState({ image: image.message });
+      console.log(this.state.image)
     } catch (error) {
       console.log(` Error fetching`);
     }
@@ -26,64 +28,19 @@ class IndexPage extends Component {
 
   render() {
     return (
-      // <div
-      //   className="index"
-      //   style={{ backgroundImage: `url(${this.state.image})` }}
-      // >
-      //   <nav className="navbar">
-      //     <a className="navbar-brand" href="to">
-      //       <i className="fas fa-dog" />
-      //     </a>
-      //   </nav>
-      //   <nav className="navbar button fixed-bottom">
-      //     <button className="btn first">View Gallery</button>
-      //   </nav>
-      // </div>
-      <div>
-        <header>
-          <a href="to" id="logo">
+      <div
+      //style={{background: `url(${this.state.image})`}}
+        id="bg"
+        className="index"
+        >
+        <nav className="navbar">
+          <a className="navbar-brand" href="to">
             <i className="fas fa-dog" />
           </a>
-        </header>
-        <section
-          style={{ backgroundImage: `url(${this.state.image})` }}
-          id="hero1"
-          className="hero"
-        >
-          <div className="inner">
-            <div className="copy">
-              <h1>Choice mountains bro</h1>
-              <p>
-                Its like im actually there! But sitting at a computer. Wow, the
-                future is intense!
-              </p>
-            </div>
-          </div>
-        </section>
-        <section className="content">
-          <div className="inner">
-            <div className="copy">
-              <h1>Amazing copy!</h1>
-              <p>The words are like jumping out at me man! </p>
-            </div>
-          </div>
-        </section>
-        <section id="hero2" className="hero">
-          <div className="inner">
-            <div className="copy">
-              <h1>Dem some big ass caves</h1>
-              <p>You could probably fit in there ;)</p>
-            </div>
-          </div>
-        </section>
-        <section className="content">
-          <div className="inner">
-            <div className="copy">
-              <h1>An inspiring quote</h1>
-              <p>/-- file not found --/</p>
-            </div>
-          </div>
-        </section>
+        </nav>
+        <nav className="navbar fixed-bottom">
+          <button className="btn first">View Gallery</button>
+        </nav>
       </div>
     );
   }

e46987e47def4c193cafc199f49cffd6e49e5aa4
Vectormike <victorjonah199@gmail.com>
2019-08-15 16:26:40 +0100

    ...

---
+++ src/containers/Gallery/gallery.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Option } from "../OptionTag/optiontag";
+
+class Breed extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Option />
+      </div>
+    );
+  }
+}
+
+export default Breed;

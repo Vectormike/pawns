722eed6e31144dc3e6d2551ac80c8eba157562d6
Vectormike <victorjonah199@gmail.com>
2019-08-15 16:27:28 +0100

    Bug here

--- src/containers/OptionTag/optiontag.jsx
+++ src/containers/OptionTag/optiontag.jsx
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { async } from "q";
 
 export function Option() {
-  const [breeds, addBreed] = useState([]);
+  let [breeds, addBreed] = useState([]);
 
   useEffect(() => {
     const fetchFunc = async () => {
       const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
-      const breeds = await response.json();
-      addBreed(breeds.message);
+      const results = await response.json();
+      addBreed(results.message);
     };
 
     fetchFunc();
   }, []);
 
-  return (
-    <div>
-      <select>
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="opel">Opel</option>
-        <option value="audi">Audi</option>
-      </select>
-    </div>
-  );
+  breeds = Array.of(breeds);
+  console.log(breeds);
+
+  let value = breeds.map((breed, i) => {
+    return (
+      <div>
+        <select>
+          <option key={i}>{breed.bulldog}</option>
+        </select>
+      </div>
+    );
+  });
+  return { value };
 }

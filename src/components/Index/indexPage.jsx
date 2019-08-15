87187c38b6582d23d228cca2f01e305828776f2a
Vectormike <victorjonah199@gmail.com>
2019-08-15 16:26:26 +0100

    ...

--- src/components/Index/indexPage.jsx
+++ src/components/Index/indexPage.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./indexPage.css";
 
-
 class IndexPage extends Component {
   constructor() {
     super();
@@ -16,7 +15,7 @@ class IndexPage extends Component {
       const response = await fetch("https://dog.ceo/api/breeds/image/random");
       const image = await response.json();
       this.setState({ image: image.message });
-      console.log(this.state.image)
+      console.log(this.state.image);
     } catch (error) {
       console.log(` Error fetching`);
     }
@@ -29,17 +28,19 @@ class IndexPage extends Component {
   render() {
     return (
       <div
-      //style={{background: `url(${this.state.image})`}}
+        //style={{background: `url(${this.state.image})`}}
         id="bg"
         className="index"
-        >
+      >
         <nav className="navbar">
           <a className="navbar-brand" href="to">
             <i className="fas fa-dog" />
           </a>
         </nav>
         <nav className="navbar fixed-bottom">
-          <button className="btn first">View Gallery</button>
+          <Link to="/breed">
+            <button className="btn first">View Gallery</button>
+          </Link>
         </nav>
       </div>
     );

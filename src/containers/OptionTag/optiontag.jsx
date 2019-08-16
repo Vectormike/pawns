866772f40d510c3f5f7821458ef416723a99f252
Vectormike <victorjonah199@gmail.com>
2019-08-16 16:09:31 +0100

    Works fine for .bulldog

--- src/containers/OptionTag/optiontag.jsx
+++ src/containers/OptionTag/optiontag.jsx
@@ -7,22 +7,23 @@ export function Option() {
     const fetchFunc = async () => {
       const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
       const results = await response.json();
-      addBreed(results.message);
+      addBreed(results.message.bulldog);
     };
 
     fetchFunc();
   }, []);
 
-  breeds = Array.of(breeds.bulldog);
   console.log(breeds);
 
   return (
     <div>
-      {breeds.map((breed, index) => (
-        <select>
-          <option key={index} value={breed}>{breed}</option>
-        </select>
-      ))}
+      <select>
+        {breeds.map((breed, index) => (
+          <option key={index} value={breed}>
+            {breed}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }

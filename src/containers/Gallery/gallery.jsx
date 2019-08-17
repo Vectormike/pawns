30cfbf9fb5b279998fdbf25cbb4d6820b4e0ee99
Vectormike <victorjonah199@gmail.com>
2019-08-17 22:28:53 +0100

    Imported Dog lists for gallery

--- src/containers/Gallery/gallery.jsx
+++ src/containers/Gallery/gallery.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Option } from "../OptionTag/optiontag";
+import List from "../DogImageList/dogimagelist";
 
 class Breed extends Component {
   render() {
     return (
       <div className="container">
         <Option />
+        <List />
       </div>
     );
   }

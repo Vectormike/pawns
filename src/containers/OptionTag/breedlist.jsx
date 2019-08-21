d316fb1d12d7568e83d49d3b620b3815a0450ea4
Vectormike <victorjonah199@gmail.com>
2019-08-21 16:57:51 +0100

    Removed state here and took it to the top

--- src/containers/OptionTag/breedlist.jsx
+++ src/containers/OptionTag/breedlist.jsx
@@ -1,31 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import "./breedlist.css";
 
-export function Option() {
-  const [breeds, addBreed] = useState([]);
+export function Option({items}) {
 
-  useEffect(() => {
-    const fetchFunc = async () => {
-      const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
-      const results = await response.json();
-      addBreed(results.message);
-    };
-
-    fetchFunc();
-  }, []);
-
-  console.log(breeds);
-  const items = [];
-  for (const prop in breeds) {
-    console.log(`${prop}`);
-    items.push(
-      <li>
-        <Link>{prop}</Link>
-      </li>
-    );
-  }
-  
   return (
     <div className="wrapper">
       <h2>Breeds</h2>

14088e155e1160e5f5cdf3b18dadb86bcb37a68c
Vectormike <victorjonah199@gmail.com>
2019-08-21 11:42:43 +0100

    Changed to list(bug still)

--- src/containers/OptionTag/optiontag.jsx
+++ src/containers/OptionTag/optiontag.jsx
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 export function Option() {
-  let [breeds, addBreed] = useState([]);
+  const [breeds, addBreed] = useState([]);
 
   useEffect(() => {
     const fetchFunc = async () => {
       const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
       const results = await response.json();
-      addBreed(results.message);
+      addBreed(Array.of(results.message));
     };
 
     fetchFunc();
   }, []);
-  breeds = Array.of(breeds);
+
   console.log(breeds);
 
   return (
-    <div>
-      <select>
-        {breeds.map((breed, index) => (
-          <option key={index} value={breed}>
-            {breed}
-          </option>
+    <article>
+      <section>
+        {breeds.map((breed, i) => (
+          <ul key={i} value={breed}>
+            <li key={i}>{breed}</li>
+          </ul>
         ))}
-      </select>
-    </div>
+      </section>
+    </article>
   );
 }

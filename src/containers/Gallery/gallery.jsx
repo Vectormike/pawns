b9da955fb1f454756c0a80958baf90cbd62c48e6
Vectormike <victorjonah199@gmail.com>
2019-08-21 16:57:32 +0100

    Brough state to top

--- src/containers/Gallery/gallery.jsx
+++ src/containers/Gallery/gallery.jsx
@@ -1,12 +1,37 @@
-import React, {} from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Option } from "../OptionTag/breedlist";
 import DogImageList from "../DogImageList/dogimagelist";
 
 function Gallery() {
-  
+  const [breeds, addBreed] = useState([]);
+
+  useEffect(() => {
+    const fetchFunc = async () => {
+      const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
+      const results = await response.json();
+      addBreed(results.message);
+    };
+
+    fetchFunc();
+  }, []);
+
+  let onClick = e => {
+    console.log("Fuckyou");
+  };
+
+  const items = [];
+  for (const prop in breeds) {
+    items.push(
+      <li>
+        <Link to="/breed" onClick={onClick}>{prop}</Link>
+      </li>
+    );
+  }
+
   return (
     <div className="container">
-      <Option />
+      <Option items={items} />
       <DogImageList />
     </div>
   );

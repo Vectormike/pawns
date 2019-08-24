08e4f438c49d5ba756ccf489b62bb6b465bd1d9d
Vectormike <victorjonah199@gmail.com>
2019-08-24 22:18:47 +0100

    ...

--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -3,16 +3,13 @@ import "./menu.css";
 
 export function Menu({ items }) {
   return (
-    <div class="container">
-      <span class="choose">Choose Breed</span>
-      <div class="dropdown">
-        <div class="select">
-          <span>Select Breed</span>
-          <i class="fa fa-chevron-left" />
+    <div>
+      <nav>
+        <div className="dropdown hover">
+          <a href="#">Hover Menu</a>
+          <ul>{items}</ul>
         </div>
-        <input type="hidden" name="gender" />
-        <ul class="dropdown-menu">{items}</ul>
-      </div>
+      </nav>
     </div>
   );
 }

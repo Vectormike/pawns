756572ac55ccf1377ec0de4077df77e204292024
Vectormike <victorjonah199@gmail.com>
2019-09-01 19:37:26 +0100

    ...

--- src/components/DogImage/dogimage.jsx
+++ src/components/DogImage/dogimage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function DogImage() {
+function DogImage({url}) {
     return (
         <div>
-            <img src="" alt=""/>
+            <img src={url} alt=""/>
         </div>
     )
 }

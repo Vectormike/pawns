20539c9df34349de3a0af3d91a895daedc13fac9
Vectormike <victorjonah199@gmail.com>
2019-09-01 23:21:20 +0100

    ...

--- src/components/DogImageList/dogimagelist.jsx
+++ src/components/DogImageList/dogimagelist.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import DogImage from "../DogImage/dogimage";
+import "./dogimagelist.css";
 
 export default function DogImageList({ image }) {
   return (
-    <div>
+    <div className="gallery">
       {image.map((res, i) => (
         <DogImage key={i} url={res} />
       ))}
